8f7e82edd32c2099906f64932a98adf2eb516145
Chiheb bidani <bidani.chiheb@yahoo.fr>
2025-08-02 16:15:27 +0200

    fix: correct XML path in PKT generation method

--- src/topologie/TopologyExecutor.py
+++ src/topologie/TopologyExecutor.py
@@ -9,5 +9,5 @@ class TopologyExecutor:
         builder = PktBuilder(base_template_path="src/resources/xml/empty.xml", devices=devices,links=links, notes=notes)
         tree = builder.inject_devices()
         builder.generateXML(tree, output_path=f"{output_path}/{output_file}.xml")
-        builder.generatePKT(xml_path=f"{output_path}/generated1.xml", output_path=f"{output_path}/{output_file}.pkt")
+        builder.generatePKT(xml_path=f"{output_path}/{output_file}.xml", output_path=f"{output_path}/{output_file}.pkt")
         print("✅ Fichier XML généré avec succès.")
